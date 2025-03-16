1b58f3f31a17244d5e89418a7ff2a8950dee4d43
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 09:47:59 +0700

    feat: Add CopyPaste1🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/sonarqubedemo/DuplicationExample.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.sonarqubedemo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-sonarqube-demo
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/03/25
+ * Time: 09.47
+ * To change this template use File | Settings | File Templates.
+ */
+public class DuplicationExample {
+    public static class CopyPaste1 {
+        private final String name;
+        private final String description;
+
+        public CopyPaste1(String name, String description) {
+            this.name = name;
+            this.description = description;
+        }
+    }
+
+}
