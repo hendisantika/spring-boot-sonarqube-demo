c3a091dbd97ca2a3eef28886351cb7dcd7eaee54
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 09:48:10 +0700

    feat: Add CopyPaste2🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/sonarqubedemo/DuplicationExample.java
+++ src/main/java/id/my/hendisantika/sonarqubedemo/DuplicationExample.java
@@ -22,4 +22,13 @@ public class DuplicationExample {
         }
     }
 
+    public static class CopyPaste2 {
+        private final String name;
+        private final String description;
+
+        public CopyPaste2(String name, String description) {
+            this.name = name;
+            this.description = description;
+        }
+    }
 }
