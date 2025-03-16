c8a71d305c35edd929c8e36cb644e66e409ec224
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 09:48:34 +0700

    feat: Add HotSpotExample🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/sonarqubedemo/HotSpotExample.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.sonarqubedemo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-sonarqube-demo
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/03/25
+ * Time: 09.48
+ * To change this template use File | Settings | File Templates.
+ */
+public class HotSpotExample {
+
+}
