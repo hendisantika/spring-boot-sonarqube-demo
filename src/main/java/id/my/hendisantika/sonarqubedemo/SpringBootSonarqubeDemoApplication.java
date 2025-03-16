7773de44553671e37e2df120f11cf43c794e9df9
hendisantika <hendisantika@yahoo.co.id>
2025-03-16 09:44:46 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/sonarqubedemo/SpringBootSonarqubeDemoApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.sonarqubedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSonarqubeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSonarqubeDemoApplication.class, args);
+    }
+
+}
